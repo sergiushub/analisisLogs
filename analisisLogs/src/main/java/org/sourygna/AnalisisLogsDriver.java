9c91654937aed8ba8203bb875e699602266b100c
sergiushub <sergio.dmgz@gmail.com>
2017-06-04 20:36:08 +0200

    Correcciones Final - añadimos try catch al reducer

--- analisisLogs/src/main/java/org/sourygna/AnalisisLogsDriver.java
+++ analisisLogs/src/main/java/org/sourygna/AnalisisLogsDriver.java
@@ -15,11 +15,11 @@ public class AnalisisLogsDriver {
 
 	public static void main(String[] args) throws Exception {
 
+		//Comprobamos que los argumentos de entrada son correctos
 		if (args.length != 2) {
 			System.out.printf("Usage: WordCount <input dir> <output dir>\n");
 			System.exit(-1);
 		}
-
 		String input = args[0];
 		String output = args[1];
 
@@ -28,17 +28,21 @@ public class AnalisisLogsDriver {
 		job.setJarByClass(AnalisisLogsDriver.class);
 		job.setJobName("Analisis Logs");
 		
+		//Definimos formato y directorio de la entrada
 		job.setInputFormatClass(TextInputFormat.class);
 		FileInputFormat.addInputPath(job, new Path(input));
-		
+
+		//Seteamos la clase mapper y el tipo de clave y valor
 		job.setMapperClass(LogMapper.class);
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(ProcessCounterWritable.class);
 		
+		//Seteamos la clase reducer y el tipo de clave y valor
 		job.setReducerClass(LogReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 
+		//Definimos formato y directorio de la salida
 		job.setOutputFormatClass(TextOutputFormat.class);
 		FileOutputFormat.setOutputPath(job, new Path(output));
 		
--- analisisLogs/src/main/java/org/sourygna/LogReducer.java
+++ analisisLogs/src/main/java/org/sourygna/LogReducer.java
@@ -1,54 +1,71 @@
 package org.sourygna;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.log4j.Logger;
 
-public class LogReducer extends Reducer<Text, ProcessCounterWritable, Text, Text> {
+public class LogReducer extends
+		Reducer<Text, ProcessCounterWritable, Text, Text> {
 
+	private static Logger logger = Logger.getLogger(LogReducer.class);
 	private Text outputKey = new Text();
 	private Text outputValue = new Text();
-	private HashMap<String,Integer> hProcess = new HashMap<String,Integer>();
+	private HashMap<String, Integer> hProcess = new HashMap<String, Integer>();
 
 	@Override
 	protected void reduce(Text key, Iterable<ProcessCounterWritable> values,
 			Reducer<Text, ProcessCounterWritable, Text, Text>.Context context)
 			throws IOException, InterruptedException {
 		Integer iCounter;
-		
-		for(ProcessCounterWritable val : values) {
-			//HacerOtroHashtable
-			iCounter = hProcess.get(val.getProcess());
-			if (iCounter == null) {
-				hProcess.put(val.getProcess(), val.getCounter());
-			} else {
-				hProcess.put(val.getProcess(), val.getCounter() + iCounter);
+
+		try {
+
+			//Recorremos los valores y creamos un hashmap donde guardaremos
+			//sumaremos los contadores de cada componente
+			for (ProcessCounterWritable val : values) {
+				iCounter = hProcess.get(val.getProcess());
+				if (iCounter == null) {
+					hProcess.put(val.getProcess(), val.getCounter());
+				} else {
+					hProcess.put(val.getProcess(), val.getCounter() + iCounter);
+				}
 			}
+
+			// Recorremos el buffer del in-mapper reducer
+			String sOutputValue = "";
+			for (Entry<String, Integer> entry : hProcess.entrySet()) {
+				sOutputValue += entry.getKey();
+				sOutputValue += ":";
+				sOutputValue += entry.getValue() + ",";
+
+				// Incrementamos el contador de componentes
+				context.getCounter("Component Counters", entry.getKey())
+						.increment(entry.getValue());
+			}
+
+			// Quitamos la ultima coma
+			sOutputValue = sOutputValue.substring(0, sOutputValue.length() - 1);
+
+			//Escribiomos la salida en context
+			outputKey.set("[" + key.toString() + "]");
+			outputValue.set(sOutputValue);
+			context.write(outputKey, outputValue);
+
+			//Limpiamos el hashmap antes de la proxima ejecucion
+			hProcess.clear();
+
+		} catch (Exception e) {
+			// Si encontramos error se muestra en el log
+			logger.error("ERRROR reducer: " + key.toString(), e);
+			// También incrementamos el contador de errores del mapper
+			context.getCounter("My Counters", "Reducer Errors").increment(1);
+			throw e;
 		}
-		
-		//Recorremos el buffer del in-mapper reducer
-		String sOutputValue = "";
-		for(Entry<String, Integer> entry: hProcess.entrySet()) {
-			sOutputValue += entry.getKey();
-			sOutputValue += ":";
-			sOutputValue += entry.getValue() + ",";
-			
-			// Incrementamos el contador de componentes
-			context.getCounter("Component Counters", entry.getKey()).increment(entry.getValue());
-		}
-		
-		//Quitamos la ultima coma
-		sOutputValue = sOutputValue.substring(0, sOutputValue.length() - 1);
-				
-		outputKey.set("[" + key.toString() + "]");
-		outputValue.set(sOutputValue);
-		
-		context.write(outputKey, outputValue);
-		
-		hProcess.clear();
 	}
-	
+
 }
